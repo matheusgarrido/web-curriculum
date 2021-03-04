1d9db09bfcf6b1ae288b6a573eded296c8624e54
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-03-04 17:23:07 -0300

    BackToTop button position

--- src/component/_footer.js
+++ src/component/_footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import BackToTop from './backToTop.js';
+
 import '../css/footer.css';
 
 export default function _header() {
   return (
     <footer>
+      <BackToTop />
       <p>Desenvolvido por Matheus Garrido &copy; 2021</p>
     </footer>
   );
---
+++ src/component/backToTop.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import '../css/backToTop.css';
+
+export default function BackToTop() {
+  return (
+    <div id="btnBackToTop">
+      <i class="fa fa-angle-up" aria-hidden="true"></i>
+    </div>
+  );
+}
