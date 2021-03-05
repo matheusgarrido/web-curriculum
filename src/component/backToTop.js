4e3a894b9642963c1a444a83d37ace1d6707847a
Matheus Garrido <matheusgarrido10@hotmail.com>
2021-03-05 12:18:56 -0300

    Return to top functional with tooltip

--- src/component/backToTop.js
+++ src/component/backToTop.js
@@ -2,8 +2,15 @@ import React, { useEffect } from 'react';
 import '../css/backToTop.css';
 
 export default function BackToTop() {
+  function returnToTop() {
+    document.documentElement.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }
   return (
-    <div id="btnBackToTop">
+    <div id="btnBackToTop" onClick={returnToTop}>
+      <p id="tooltipButton">Voltar para o topo</p>
       <i class="fa fa-angle-up" aria-hidden="true"></i>
     </div>
   );
